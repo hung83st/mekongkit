f32e0fbcfc5488e67c22ca9f19fed64b62eb22c4
hung83st <hung84618@gmail.com>
2025-10-31 00:42:07 +0700

    Update app.py

--- travel_kit/app.py
+++ travel_kit/app.py
@@ -149,10 +149,6 @@ def cho_noi_cai_rang():
 def den_vua_hung():
     return render_template('den_vua_hung.html')
 
-# Địa điểm của dân tộc Hoa
-@app.route("/chua_ba_thien_hau")
-def chua_ba_thien_hau():
-    return render_template("chua_ba_thien_hau.html")
 
 # --- Chạy app ---
 if __name__ == '__main__':
